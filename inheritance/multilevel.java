62e0db42f8b0b2f302b269e6b3914b9f3fbf7ec3
GAGANDASH002 <gagandash002@gmail.com>
2024-03-14 12:56:29 +0530

    multi level inheritance

---
+++ inheritance/multilevel.java
@@ -0,0 +1,61 @@
+
+
+
+class A{
+    int a;
+    A(int a)
+    {
+        this.a=a;
+    }
+    void show()
+    {
+        System.out.println(a);
+    }
+}
+
+class B extends A{
+    int b;/*b is a data member of subclass B */
+    B(int a,int b)
+    {
+        super(a);/*super() is used to initialize superclass constructor */
+        this.b=b;
+    }
+    void show()
+    {
+        System.out.println(a+""+b);
+    }
+}
+
+class C extends B{
+    int c;
+    C(int a ,int b,int c)
+    {
+        super(a,b);
+        this.c=c;
+    }
+    void show()
+    {
+        System.out.println(a+""+b+""+c);
+    }
+}
+
+class demo{
+    public static void main(String[] args) {
+        A a = new A (1);
+        System.out.println("objects of class A");
+        a.show();
+
+        A b = new B(1,2);
+        System.out.println("objects of class B");
+        b.show(); /*subclass inherits data members and methods of superclass */
+
+        B c = new C(1,2,3);
+        System.out.println("objects of class C");
+        c.show();
+    }
+}
+
+
+
+
+
