7c370dc1a6790a5e30357ea189f7047f939acb07
GAGANDASH002 <gagandash002@gmail.com>
2024-03-14 12:26:20 +0530

    throw statement

---
+++ throw.java
@@ -0,0 +1,23 @@
+package java;
+
+class Demo2{
+    public static void main(String[] args) {
+        int a=19;
+        int b;
+        try{
+            b=18/a;
+            System.out.println(b);
+            if(b==0){
+                throw new ArithmeticException();
+            }
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Division by zero !!!");
+        }
+        finally{
+            System.out.println("Okay Finally done");
+        }
+    }
+    
+}
