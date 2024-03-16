5771e1a087189a33e8a5ec5780c03c93824041bf
GAGANDASH002 <gagandash002@gmail.com>
2024-03-16 18:08:08 +0530

    question 2

--- exceptions/argument.java
+++ exceptions/argument.java
@@ -1,4 +1,8 @@
 package java.exceptions;
+/*Write an exception class named checkargument() which returns exception if the number of command line 
+argumentsare less than 4. Create an application class which throws the checkargument exception if
+the number of command line arguments entered are less than 4
+ */
 
 class checkargument extends Exception{
     checkargument(){
@@ -12,9 +16,15 @@ class checkargument extends Exception{
 }
 
 class argument{
-    public static void main(String y[]){
+    public static void main(String s[]){
+
         try{
-            if(y.length<4){
+            int w,x,y,z;
+            w = Integer.parseInt(s[0]);
+            x = Integer.parseInt(s[1]);
+            y = Integer.parseInt(s[2]);
+            z = Integer.parseInt(s[3]);
+            if(s.length<4){
                 throw new checkargument();
             }
         }
