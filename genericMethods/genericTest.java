3dcc913a44f12d34e2c0a6aa05a27b4bcd1c7530
GAGANDASH002 <gagandash002@gmail.com>
2024-07-22 21:23:01 +0530

    genericPrint method example in Java

---
+++ genericMethods/genericTest.java
@@ -0,0 +1,11 @@
+class Demo{
+    <T> void genericPrint(T t){
+        System.out.println(t);
+    }
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.genericPrint(101);// Calling method with integer argument
+        obj.genericPrint("Hello World"); // Calling method with string argument
+        obj.genericPrint(2.005432); // Calling method with float argument
+    }
+}
